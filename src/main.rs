b0b1091e99855cd1212ebd0c72a45bcd2e8f52db
Drew Zemke <drew.zemke@gmail.com>
2023-05-14 21:51:48 -0700

    refactor(render): moved scene/camera setup into main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 use image::{ImageBuffer, Rgb};
-use rays::render::hello_sphere;
+use rays::{
+    camera::Camera,
+    math::vec3::Vec3,
+    render::render,
+    scene::{object::sphere::Sphere, Scene},
+};
 
 fn main() {
     // window setup
     let output_width = 800;
     let output_height = 500;
-    let color_mat = hello_sphere(output_width, output_height);
+
+    // scene setup
+    let scene = Scene::new(vec![
+        Sphere::new(1.0, Vec3::new(1.0, 1.0, 0.0)),
+        Sphere::new(0.5, Vec3::new(-1.0, 0.5, -2.0)),
+        Sphere::new(0.5, Vec3::new(-2.0, 0.5, 1.0)),
+        Sphere::new(5.0, Vec3::new(5.0, 5.0, -5.0)),
+    ]);
+
+    // camera setup
+    // QUESTION: should this be part of scene?
+    let camera = Camera::new(
+        output_width,
+        output_height,
+        4.0,
+        4.0,
+        Vec3::new(0.0, 1.0, 4.0),
+    );
+
+    // render
+    let color_mat = render(scene, camera, output_width, output_height);
 
     // write to output
-
     let img_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = color_mat.into();
     img_buffer.save("target/debug/img_out/render.png").unwrap();
 }
--- src/render.rs
+++ src/render.rs
@@ -1,27 +1,8 @@
-use crate::{
-    camera::Camera,
-    math::{color::ColorMatrix, vec3::Vec3},
-    scene::{object::sphere::Sphere, Scene},
-};
+use crate::{camera::Camera, math::color::ColorMatrix, scene::Scene};
 
-pub fn hello_sphere(output_width: u32, output_height: u32) -> ColorMatrix {
-    let camera = Camera::new(
-        output_width,
-        output_height,
-        4.0,
-        4.0,
-        Vec3::new(0.0, 1.0, 4.0),
-    );
-
+pub fn render(scene: Scene, camera: Camera, output_width: u32, output_height: u32) -> ColorMatrix {
     // create output
     let mut color_mat = ColorMatrix::new(output_width as usize, output_height as usize);
-
-    let scene = Scene::new(vec![
-        Sphere::new(1.0, Vec3::new(1.0, 1.0, 0.0)),
-        Sphere::new(0.5, Vec3::new(-1.0, 0.5, -2.0)),
-        Sphere::new(0.5, Vec3::new(-2.0, 0.5, 1.0)),
-        Sphere::new(5.0, Vec3::new(5.0, 5.0, -5.0)),
-    ]);
 
     // compute pixel values
     for pixel_x in 0..output_width {
