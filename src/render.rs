c2fbc5fab25e459d50e8fd66ae68211add141cc3
Drew Zemke <drew.zemke@gmail.com>
2023-05-14 17:38:29 -0700

    refactor(scene): manages ray/object interaction

--- src/render.rs
+++ src/render.rs
@@ -1,12 +1,7 @@
 use crate::{
     camera::Camera,
-    math::{
-        color::{Color, ColorMatrix},
-        ray::{IntersectRay, Intersection},
-        shaping::lerp,
-        vec3::Vec3,
-    },
-    scene::object::sphere::Sphere,
+    math::{color::ColorMatrix, vec3::Vec3},
+    scene::Scene,
 };
 
 pub fn hello_sphere(output_width: u32, output_height: u32) -> ColorMatrix {
@@ -18,14 +13,10 @@ pub fn hello_sphere(output_width: u32, output_height: u32) -> ColorMatrix {
         Vec3::new(0.0, 0.0, 2.0),
     );
 
-    let sphere = Sphere::new(1.0, Vec3::new(1.0, 0.0, 0.0));
-
-    // sky color
-    let zenith_col = Color::from_rgb(1.0, 1.0, 1.0);
-    let nadir_col = Color::from_rgb(0.5, 0.7, 1.0);
-
     // create output
     let mut color_mat = ColorMatrix::new(output_width as usize, output_height as usize);
+
+    let scene = Scene {};
 
     // compute pixel values
     for pixel_x in 0..output_width {
@@ -33,20 +24,7 @@ pub fn hello_sphere(output_width: u32, output_height: u32) -> ColorMatrix {
             let ray = camera.ray_for_pixel(pixel_x, pixel_y);
 
             let mat_entry = color_mat.at_mut(pixel_y as usize, pixel_x as usize);
-
-            match sphere.intersect_ray(&ray) {
-                Some(Intersection { point: _, normal }) => {
-                    let mapped_normal = 0.5 * (normal + Vec3::new(1.0, 1.0, 1.0));
-                    *mat_entry = mapped_normal.into();
-                }
-                None => {
-                    // TODO: remap!!
-                    let t = 0.5 * (ray.dir.y + 1.0);
-
-                    let sky_color = lerp(t, zenith_col, nadir_col);
-                    *mat_entry = sky_color;
-                }
-            }
+            *mat_entry = scene.color_for_ray(ray)
         }
     }
 
--- src/scene.rs
+++ src/scene.rs
@@ -1,8 +1,38 @@
 use self::object::sphere::Sphere;
+use crate::math::{
+    color::Color,
+    ray::{IntersectRay, Intersection, Ray},
+    shaping::lerp,
+    vec3::Vec3,
+};
 
 pub mod object;
 
 #[derive(Debug)]
 pub struct Scene {
-    objects: Vec<Sphere>,
+    // objects: Vec<Sphere>,
+}
+
+impl Scene {
+    pub fn color_for_ray(&self, ray: Ray) -> Color {
+        // let sphere = &self.objects[0];
+
+        let sphere = Sphere::new(1.0, Vec3::new(1.0, 0.0, 0.0));
+
+        // sky color
+        let zenith_col = Color::from_rgb(1.0, 1.0, 1.0);
+        let nadir_col = Color::from_rgb(0.5, 0.7, 1.0);
+
+        match sphere.intersect_ray(&ray) {
+            Some(Intersection { point: _, normal }) => {
+                let mapped_normal = 0.5 * (normal + Vec3::new(1.0, 1.0, 1.0));
+                mapped_normal.into()
+            }
+            None => {
+                // TODO: remap!!
+                let t = 0.5 * (ray.dir.y + 1.0);
+                lerp(t, zenith_col, nadir_col)
+            }
+        }
+    }
 }
