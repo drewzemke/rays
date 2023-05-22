38128481991dc79b2f7a0fad4860954d3a4145e8
Drew Zemke <drew.zemke@gmail.com>
2023-05-21 21:47:07 -0700

    fix(material): stop fuzzy metal reflections that point into surface

--- src/scene.rs
+++ src/scene.rs
@@ -55,12 +55,11 @@ impl<'a> Scene<'a> {
                     .material
                     .scatter_ray(&ray, intersection);
 
-                // TODO:
-                // reject this (return black or something) if the scattered day points opposite (negative dot product)
+                // absorb this ray (return black) if the scattered ray points opposite (negative dot product)
                 // the normal of the surface
-                // if Vec3::dot(&scattered_ray.dir, &intersection.normal) < 0.0 {
-                //
-                // }
+                if Vec3::dot(&scattered_ray.dir, &intersection.normal) < 0.0 {
+                    return Color::from_rgb_u8(0, 0, 0);
+                }
                 reflection_color * &self.color_for_ray(scattered_ray, bounce_depth - 1)
             }
             None => self.sky_color_for_direction(ray.dir),
