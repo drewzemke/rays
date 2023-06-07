7de546e8760fe5b7e1670f515b84db0923d39a21
Drew Zemke <drew.zemke@gmail.com>
2023-06-06 22:25:06 -0700

    chore(saving): testing writing scenes to json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use image::{ImageBuffer, Rgb};
 use rand::{rngs::StdRng, Rng, SeedableRng};
 use rays::{
@@ -22,6 +24,116 @@ fn main() {
     let output_height = 500;
 
     // scene setup
+    let scene = make_initial_test_scene();
+
+    // camera setup
+    // QUESTION: should this be part of scene?
+    let camera = Camera::new(
+        Vec3::new(13.0, 1.5, 3.0),
+        Vec3::new(0.0, 0.5, 0.0),
+        30.0,
+        10.0,
+        0.07,
+        output_width,
+        output_height,
+    );
+
+    // write_to_json(&scene, "test.json")
+
+    // render
+    let samples_per_pixel = 20;
+    let bounce_depth = 10;
+
+    let color_mat = render(
+        scene,
+        camera,
+        output_width,
+        output_height,
+        samples_per_pixel,
+        bounce_depth,
+    );
+
+    // write to output
+    let img_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = color_mat.into();
+    img_buffer.save("target/debug/img_out/render.png").unwrap();
+}
+
+const SCENE_PATH: &str = "./data";
+
+fn write_to_json(scene: &Scene, name: &str) {
+    fs::write(
+        format!("{}/{}", SCENE_PATH, name),
+        serde_json::to_vec(scene).unwrap(),
+    )
+    .unwrap();
+}
+
+// temporary until I'm happy with serde usage
+fn make_initial_test_scene() -> Scene {
+    let mut scene = Scene::new();
+
+    let sphere0 = Sphere::new(1.0, Vec3::new(1.0, 1.0, 0.0));
+    let sphere1 = Sphere::new(0.5, Vec3::new(-1.0, 0.5, -2.0));
+    let sphere2 = Sphere::new(0.5, Vec3::new(-2.0, 0.5, 1.0));
+    let sphere3 = Sphere::new(5.0, Vec3::new(5.0, 5.0, -5.0));
+    let sphere4 = Sphere::new(0.75, Vec3::new(-0.75, 0.75, 2.0));
+    let mut sphere5 = Sphere::new(0.65, Vec3::new(-0.75, 0.75, 2.0));
+    sphere5.flip_orientation();
+
+    // let plane = Plane::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 1.0, 0.0));
+    let plane = Plane::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 1.0, 0.0));
+
+    let lambert_gray = Lambertian::new(Color::from_rgb_f32(0.5, 0.5, 0.5));
+    let lambert_pink = Lambertian::new(Color::from_rgb_u8(255, 121, 198));
+    // let lambert_purple = Lambertian::new(Color::from_rgb_u8(189, 147, 249));
+    let lambert_green = Lambertian::new(Color::from_rgb_u8(80, 250, 123));
+    let metal_yellow = Metal::new(Color::from_rgb_u8(241, 250, 140), 0.4);
+    let metal_orange = Metal::new(Color::from_rgb_u8(255, 184, 108), 0.3);
+
+    let glass = Translucent::new(1.5);
+
+    let object0 = Object {
+        geometry: Box::new(sphere0),
+        material: Box::new(lambert_pink),
+    };
+    let object1 = Object {
+        geometry: Box::new(sphere1),
+        material: Box::new(lambert_green),
+    };
+    let object2 = Object {
+        geometry: Box::new(sphere2),
+        material: Box::new(metal_yellow),
+    };
+    let object3 = Object {
+        geometry: Box::new(sphere3),
+        material: Box::new(metal_orange),
+    };
+    let object4 = Object {
+        geometry: Box::new(plane),
+        material: Box::new(lambert_gray),
+    };
+    let object5 = Object {
+        geometry: Box::new(sphere4),
+        material: Box::new(glass.clone()),
+    };
+    let object6 = Object {
+        geometry: Box::new(sphere5),
+        material: Box::new(glass),
+    };
+
+    scene.add(object0);
+    scene.add(object1);
+    scene.add(object2);
+    scene.add(object3);
+    scene.add(object4);
+    scene.add(object5);
+    scene.add(object6);
+
+    scene
+}
+
+// temporary until I'm happy with serde usage
+fn make_tutorial_end_scene() -> Scene {
     let mut scene = Scene::new();
 
     let sphere0 = Sphere::new(1.0, Vec3::new(0.0, 1.0, 0.0));
@@ -85,34 +197,7 @@ fn main() {
         }
     }
 
-    // camera setup
-    // QUESTION: should this be part of scene?
-    let camera = Camera::new(
-        Vec3::new(13.0, 1.5, 3.0),
-        Vec3::new(0.0, 0.5, 0.0),
-        30.0,
-        10.0,
-        0.07,
-        output_width,
-        output_height,
-    );
-
-    // render
-    let samples_per_pixel = 20;
-    let bounce_depth = 10;
-
-    let color_mat = render(
-        scene,
-        camera,
-        output_width,
-        output_height,
-        samples_per_pixel,
-        bounce_depth,
-    );
-
-    // write to output
-    let img_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = color_mat.into();
-    img_buffer.save("target/debug/img_out/render.png").unwrap();
+    scene
 }
 
 // TODO: find a better way to manage this!
@@ -151,60 +236,3 @@ fn main() {
 // };
 
 // let scene = Scene::new(vec![&object0, &object1, &object2, &object3, &object4]);
-
-//
-//
-//
-// initial test scene
-// let sphere0 = Sphere::new(1.0, Vec3::new(1.0, 1.0, 0.0));
-// let sphere1 = Sphere::new(0.5, Vec3::new(-1.0, 0.5, -2.0));
-// let sphere2 = Sphere::new(0.5, Vec3::new(-2.0, 0.5, 1.0));
-// let sphere3 = Sphere::new(5.0, Vec3::new(5.0, 5.0, -5.0));
-// let sphere4 = Sphere::new(0.75, Vec3::new(-0.75, 0.75, 2.0));
-// let mut sphere5 = Sphere::new(0.65, Vec3::new(-0.75, 0.75, 2.0));
-// sphere5.flip_orientation();
-
-// // let plane = Plane::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 1.0, 0.0));
-// let plane = Plane::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 1.0, 0.0));
-
-// let lambert_gray = Lambertian::new(Color::from_rgb_f32(0.5, 0.5, 0.5));
-// let lambert_pink = Lambertian::new(Color::from_rgb_u8(255, 121, 198));
-// // let lambert_purple = Lambertian::new(Color::from_rgb_u8(189, 147, 249));
-// let lambert_green = Lambertian::new(Color::from_rgb_u8(80, 250, 123));
-// let metal_yellow = Metal::new(Color::from_rgb_u8(241, 250, 140), 0.4);
-// let metal_orange = Metal::new(Color::from_rgb_u8(255, 184, 108), 0.3);
-
-// let glass = Translucent::new(1.5);
-
-// let object0 = Object {
-//     geometry: &sphere0,
-//     material: &lambert_pink,
-// };
-// let object1 = Object {
-//     geometry: &sphere1,
-//     material: &lambert_green,
-// };
-// let object2 = Object {
-//     geometry: &sphere2,
-//     material: &metal_yellow,
-// };
-// let object3 = Object {
-//     geometry: &sphere3,
-//     material: &metal_orange,
-// };
-// let object4 = Object {
-//     geometry: &plane,
-//     material: &lambert_gray,
-// };
-// let object5 = Object {
-//     geometry: &sphere4,
-//     material: &glass,
-// };
-// let object6 = Object {
-//     geometry: &sphere5,
-//     material: &glass,
-// };
-
-// let scene = Scene::new(vec![
-//     &object0, &object1, &object2, &object3, &object4, &object5, &object6,
-// ]);
