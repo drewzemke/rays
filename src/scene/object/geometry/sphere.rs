d3431959b101fc30f7e840d03a0017dbac117d2d
Drew Zemke <drew.zemke@gmail.com>
2023-06-11 18:02:34 -0700

    refactor(sphere): reorganized/commented sphere intersection method

--- src/scene/object/geometry/sphere.rs
+++ src/scene/object/geometry/sphere.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 
 use crate::math::{ray::Ray, vec3::Vec3};
-
 use crate::scene::object::geometry::{IntersectRay, Intersection};
 
 use super::NormalOrientation;
@@ -32,54 +31,49 @@ impl Sphere {
 #[typetag::serde]
 impl IntersectRay for Sphere {
     fn intersect_ray(&self, ray: &Ray) -> Option<Intersection> {
-        let a = &ray.origin;
-        let b = &ray.dir;
-        let c = &self.center;
-        let r = self.radius;
-        let a_min_c = a - c;
+        // the displacement vector from the sphere center to the origin of the ray
+        let disp = (&ray.origin) - (&self.center);
 
         // quadratic equation coefficients
-        let q_a = Vec3::dot(b, b);
-        let q_b = 2.0 * Vec3::dot(b, &a_min_c);
-        let q_c = Vec3::dot(&a_min_c, &a_min_c) - r * r;
+        let q_a = Vec3::dot(&ray.dir, &ray.dir);
+        let q_b = 2.0 * Vec3::dot(&ray.dir, &disp);
+        let q_c = Vec3::dot(&disp, &disp) - self.radius.powi(2);
 
         let disc = q_b * q_b - 4.0 * q_a * q_c;
         if disc >= 0.0 {
-            // if ||a-c|| ~= r, the ray originated inside the sphere,
-            // and if the (a-c).b is negative then the ray is pointing into the sphere,
-            // so the larger of the two solutions to the quadratic equation is the
-            // only positive solution
-            let t;
-            let mut into_surface;
-
+            // NOTE: if |disp| ~= r, the ray originated on the the sphere itself,
+            // and if (disp).dir is negative then the ray is pointing into the sphere,
             // HACK: subtracting the 0.0001 dodges some floating point oddities
             //   it'd be better to have a more global / general solution
-            if q_c.abs() < 0.0001 && q_b < 0.0 {
-                t = (-q_b + disc.powf(0.5)) / (2.0 * q_a);
-                into_surface = false;
-            } else {
-                t = (-q_b - disc.powf(0.5)) / (2.0 * q_a);
-                into_surface = true;
-            }
+            // FIXME: we're not handling the case where the ray starts properly inside the sphere
+            let ray_starts_in_sphere = q_c.abs() < 0.0001 && q_b < 0.0;
 
-            // otherwise, the smaller of the two solutions is the first one along the ray
+            let is_into_surface = match self.orientation {
+                NormalOrientation::Outward => !ray_starts_in_sphere,
+                NormalOrientation::Inward => ray_starts_in_sphere,
+            };
+
+            let t = if ray_starts_in_sphere {
+                // the first solution will be negative or close to zero, so the larger of the two
+                // solutions to the quadratic equation is the only positive solution
+                (-q_b + disc.powf(0.5)) / (2.0 * q_a)
+            } else {
+                // the ray passes through the sphere twice, so the smaller of the two solutions
+                // is the first one along the ray
+                (-q_b - disc.powf(0.5)) / (2.0 * q_a)
+            };
 
             let point = ray.at(t);
-            let normal_sign = match self.orientation {
-                NormalOrientation::Outward => 1.0,
-                NormalOrientation::Inward => {
-                    // HACK: oh god is this a hack
-                    // really need to find a better way to sort out how the orientation and `into_surface` interact
-                    into_surface = !into_surface;
-                    -1.0
-                }
+            let normal = match self.orientation {
+                NormalOrientation::Outward => (&point - (&self.center)).normalize(),
+                NormalOrientation::Inward => ((&self.center) - &point).normalize(),
             };
-            let normal = normal_sign * &(&point - c).normalize();
+
             Some(Intersection {
                 point,
                 normal,
                 t,
-                is_into_surface: into_surface,
+                is_into_surface,
             })
         } else {
             None
